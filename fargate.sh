2fd3f635618fa598d9d9a8223e5fa6dd08132ebc
Giovanni Gargiulo <ggargiulo@gilt.com>
2017-12-14 21:21:44 +0000

    Initial commit

---
+++ fargate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+SELF=$(basename $0)
+UPDATE_BASE=http://something
+
+runSelfUpdate() {
+  echo "Performing self-update..."
+
+  # Download new version
+  echo -n "Downloading latest version..."
+  if ! wget --quiet --output-document="$0.tmp" $UPDATE_BASE/$SELF ; then
+    echo "Failed: Error while trying to wget new version!"
+    echo "File requested: $UPDATE_BASE/$SELF"
+    exit 1
+  fi
+  echo "Done."
+
+  # Copy over modes from old version
+  OCTAL_MODE=$(stat -c '%a' $SELF)
+  if ! chmod $OCTAL_MODE "$0.tmp" ; then
+    echo "Failed: Error while trying to set mode on $0.tmp."
+    exit 1
+  fi
+
+  # Spawn update script
+  cat > updateScript.sh << EOF
+#!/bin/bash
+# Overwrite old file with new
+if mv "$0.tmp" "$0"; then
+  echo "Done. Update complete."
+  rm \$0
+else
+  echo "Failed!"
+fi
+EOF
+
+  echo -n "Inserting update process..."
+  exec /bin/bash updateScript.sh
+}
+
+runSelfUpdate
+
